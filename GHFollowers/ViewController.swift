9411862b1cac3a4d0077e5f2ed51f536e16e8a52
Zohra <guliyevaazohra@gmail.com>
2024-04-18 12:09:41 +0400

    commit
    first step

---
+++ GHFollowers/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  GHFollowers
+//
+//  Created by Zohra Guliyeva  on 18.04.24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
