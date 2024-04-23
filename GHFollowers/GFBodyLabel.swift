8ba187659c46a3a3d5da2da91c9903435bacb7ff
Zohra <guliyevaazohra@gmail.com>
2024-04-23 09:44:58 +0400

    commit

--- GHFollowers/GFBodyLabel.swift
+++ GHFollowers/GFBodyLabel.swift
@@ -9,5 +9,30 @@ import UIKit
 
 class GFBodyLabel: UILabel {
 
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        configure()
+    }
+    
+    
+    required init?(coder: NSCoder) { //stroyboard initializer
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    
+    init(textAlignMent: NSTextAlignment){
+        super.init(frame: .zero)
+        self.textAlignment = textAlignMent
+        configure()
+    }
+    
+    private func configure() {
+        textColor = .secondaryLabel
+        adjustsFontSizeToFitWidth = true
+        font = UIFont.preferredFont(forTextStyle: .body )
+        minimumScaleFactor = 0.75 ///Metin boyutunun ne kadar küçültülebileceğini belirler.
+        lineBreakMode = .byWordWrapping
+        translatesAutoresizingMaskIntoConstraints = false
+    }
 
 }
