721a3fc35546f562e19f6f15f9a2bcf2b9d7f36d
Zohra <guliyevaazohra@gmail.com>
2024-04-25 11:38:30 +0400

    commit
    folder structure

---
+++ GHFollowers/Model/Follower.swift
@@ -0,0 +1,14 @@
+//
+//  Follower.swift
+//  GHFollowers
+//
+//  Created by Zohra Guliyeva  on 25.04.24.
+//
+
+import Foundation
+
+struct Follower: Codable {
+    var login: String
+    var avatar_url: String
+    
+}
